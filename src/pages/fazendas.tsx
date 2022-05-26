a04df79471202f7e602f7e4e69d8a6393c71f8b4
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2022-05-26 11:10:56 -0300

    feat: favicon

--- src/pages/fazendas.tsx
+++ src/pages/fazendas.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import React from 'react'
 import { FarmDashboard } from '../components/FarmDashboard'
 
 function Fazendas() {
   return (
     <>
+      <Head>
+        <title>Fazendas</title>
+      </Head>
       <FarmDashboard />
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import React from 'react'
 import { Dashboard } from '../components/Dashboard'
 
 function Home() {
   return (
     <>
+      <Head>
+        <title>Recibos</title>
+      </Head>
       <Dashboard />
     </>
   )
